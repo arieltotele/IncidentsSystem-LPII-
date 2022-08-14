b9456d27e05bff9750058d87271b14e8022a4083
Arieltotele <arielasbel@hotmail.com>
2022-08-14 14:37:13 -0700

    Firs commit for main branch

--- Proyecto Final de lenguaje de programacion 2/Program.cs
+++ Proyecto Final de lenguaje de programacion 2/Program.cs
@@ -8,9 +8,7 @@ namespace Proyecto_Final_de_lenguaje_de_programacion_2
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
