4e4894541e84a24a2c55e85f46a77a7b451e9145
Arieltotele <arielasbel@hotmail.com>
2022-08-15 10:46:08 -0700

    Generic Repository created and implemented.

---
+++ Proyecto Final de lenguaje de programacion 2/Interfaces/OperationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_de_lenguaje_de_programacion_2.Interfaces
+{
+    public class OperationResult
+    {
+        public bool Succes { get; set; }
+        public String Message { get; set; }
+    }
+}
